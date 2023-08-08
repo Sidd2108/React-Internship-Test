74279cb41b849215e5e0585ab3fabc4fb79635d6
Siddhesh Shinde <talktosiddhesh.shinde@gmail.com>
2023-08-08 20:20:10 +0530

    Final Changes

--- src/components/Show.jsx
+++ src/components/Show.jsx
@@ -1,9 +1,14 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect, useRef, useState } from 'react'
+import { Navigate, useParams } from 'react-router-dom'
 
 const Show = () => {
     const { name } = useParams();
     const [show, setShow] = useState([]);
+    const [clicked, setClicked] = useState(false);
+    const [title, setTitle] = useState("");
+    const [redirect, setRedirect] = useState(false);
+    const nameRef = useRef("");
+    const numberRef = useRef(0);
     async function getData() {
         const response = await fetch(`https://api.tvmaze.com/singlesearch/shows?q=${name}`);
         const data = await response.json();
@@ -12,10 +17,48 @@ const Show = () => {
 
     useEffect(() => {
         getData();
+
     }, []);
     function handleClick() {
-        alert("Your Ticket is booked");
+        setClicked(true);
+        setTitle(show.name);
     }
+
+    function handleSubmit(ev) {
+        ev.preventDefault();
+        const bookingDetails = {
+            titleoFShow: title,
+            name: nameRef.current.value,
+            numberOfTickets: numberRef.current.value
+        }
+        localStorage.setItem(nameRef.current.value, JSON.stringify(bookingDetails));
+        alert("Booking Successful. See Browser Local Storage for booking details");
+        setRedirect(true);
+    }
+    if (redirect) {
+        return <Navigate to={"/"} />
+    }
+
+    if (clicked) {
+        return (
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <label>Title of Show</label>
+                    <input type="text" value={title} readOnly /> <br />
+
+                    <label>Your Name</label>
+                    <input ref={nameRef} type="text" /> <br />
+
+                    <label> No of Tickets</label>
+                    <input ref={numberRef} type="number" /> <br />
+                    <button>Book your Ticket</button>
+
+                </form>
+            </div>
+        )
+    }
+
+
     return (
         <div className='individual-show'>
             <h1>Title : {show.name}</h1>
